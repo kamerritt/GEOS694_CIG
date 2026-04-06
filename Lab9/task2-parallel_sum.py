83b8f4698ca5620b8e479eaafbc66cb6a19dfec6
Karenna Merritt <kamerritt@alaska.edu>
2026-04-06 15:47:09 -0800

    task 2 complete

--- Lab9/task2-parallel_sum.py
+++ Lab9/task2-parallel_sum.py
@@ -5,15 +5,24 @@ import numpy as np
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-reduced = None
+num_data = 1000
+data = None
     
 if rank == 0:
-    data = np.arange(1, 20)
+    data = np.arange(1, num_data+1)
+    #print(data)
+    partial = np.array_split(data, size)
+    #print(partial)
+else:
+    partial = None 
+    
+part = comm.scatter(partial, root=0)
 
-partial = np.empty(len(data)/size, dtype='d')
-comm.Scatter(data, partial, root=0)
+partial = sum(part)
+print('Partial sum = ', partial)
 
-if rank in range(2, 5):
-    partial = sum(partial)
+tot = comm.gather(partial, root=0)
 
-comm.Gather(sum(partial), reduced, root=0)
+if rank == 0:
+    overall_sum = sum(tot)
+    print('Total sum = ', overall_sum)
